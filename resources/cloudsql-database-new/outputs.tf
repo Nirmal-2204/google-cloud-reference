773b52b65fa724c7eb7a65aaf0474130dc3ed7b8
Nirmal-2204 <100931962+Nirmal-2204@users.noreply.github.com>
2024-07-09 20:50:46 +0530

    Update outputs.tf

--- resources/cloudsql-database-new/outputs.tf
+++ resources/cloudsql-database-new/outputs.tf
@@ -1,21 +1,28 @@
+# See https://developer.humanitec.com/platform-orchestrator/reference/resource-types/#postgres for expected outputs
+
 output "host" {
-  value = data.google_sql_database_instance.instance.private_ip_address
-}
-
-output "port" {
-  value = 5432
-}
-
-output "username" {
-  value     = google_sql_user.user.name
-  sensitive = true
-}
-
-output "password" {
-  value     = google_sql_user.user.password
-  sensitive = true
+  description = "The IP address the instance is available on."
+  value       = data.google_sql_database_instance.instance.private_ip_address
 }
 
 output "name" {
-  value     = google_sql_database.database.name
+  description = "The name of the database that the workload should connect to."
+  value       = google_sql_database.main.name
+}
+
+output "port" {
+  description = "The port on the host that the instance is available on."
+  value       = 5432
+}
+
+output "username" {
+  description = "The user that the workload should use to connect to the database."
+  value       = google_sql_user.main.name
+  sensitive   = true
+}
+
+output "password" {
+  description = "The password for the user."
+  value       = google_sql_user.main.password
+  sensitive   = true
 }
